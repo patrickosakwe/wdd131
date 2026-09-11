de08b8f8e473e8073ece3fc8c61fab27945731ea
patrickosakwe <posakwe@byupathway.edu>
2026-09-11 21:08:28 +0100

    add

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,8 +2,13 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("#submit");
 const list = document.querySelector("#list");
 
-
 button.addEventListener("click", function () {
+
+    if (input.value.trim() === "") {
+        input.focus();
+        return;
+    }
+
     const li = document.createElement("li");
     const deleteButton = document.createElement("button");
 
@@ -13,5 +18,11 @@ button.addEventListener("click", function () {
     li.appendChild(deleteButton);
     list.appendChild(li);
 
+    deleteButton.addEventListener("click", function () {
+        li.remove();
+    });
+
     input.value = "";
+
+    input.focus();
 });
